be3b56c8c4f6a3a9062d803702357c11ba71209b
Emmanuel Méndez <emmy.mp.98e@gmail.com>
2020-09-29 19:48:58 -0500

    correccion minusculas

--- main.go
+++ main.go
@@ -11,31 +11,29 @@ func main() {
 
 	switch {
 	case (mes == 12 && dia >= 22) || (mes == 1 && dia <= 20):
-		fmt.Println("Capricornio")
+		fmt.Println("capricornio")
 	case (mes == 1 && dia >= 21) || (mes == 2 && dia <= 18):
-		fmt.Println("Acuario")
+		fmt.Println("acuario")
 	case (mes == 2 && dia >= 19) || (mes == 3 && dia <= 20):
-		fmt.Println("Piscis")
+		fmt.Println("piscis")
 	case (mes == 3 && dia >= 21) || (mes == 4 && dia <= 20):
-		fmt.Println("Aries")
+		fmt.Println("aries")
 	case (mes == 4 && dia >= 21) || (mes == 5 && dia <= 20):
-		fmt.Println("Tauro")
+		fmt.Println("tauro")
 	case (mes == 5 && dia >= 21) || (mes == 6 && dia <= 21):
-		fmt.Println("Géminis")
+		fmt.Println("geminis")
 	case (mes == 6 && dia >= 22) || (mes == 7 && dia <= 22):
-		fmt.Println("Cáncer")
+		fmt.Println("cancer")
 	case (mes == 7 && dia >= 23) || (mes == 8 && dia <= 22):
-		fmt.Println("Leo")
+		fmt.Println("leo")
 	case (mes == 8 && dia >= 23) || (mes == 9 && dia <= 22):
-		fmt.Println("Virgo")
+		fmt.Println("virgo")
 	case (mes == 9 && dia >= 23) || (mes == 10 && dia <= 22):
-		fmt.Println("Libra")
+		fmt.Println("libra")
 	case (mes == 10 && dia >= 23) || (mes == 11 && dia <= 22):
-		fmt.Println("Escorpio")
+		fmt.Println("escorpio")
 	case (mes == 11 && dia >= 23) || (mes == 12 && dia <= 21):
-		fmt.Println("Sagitario")
-	default:
-		fmt.Println("Error de fecha")
+		fmt.Println("sagitario")
 	}
 
 }
